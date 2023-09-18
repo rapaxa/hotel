bd003a4940e460487b47f10d2b517f9e3fdcb445
pavelvolodcenko <rapaxa123@gmail.com>
2023-09-19 00:09:46 +0100

    sign

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css';
-import AuthGoogle from "./components/Author/AuthGoogle";
-import Register from "./components/Author/RegistrNewUser";
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import MainLayout from "./pages/MainLayout/MainLayout";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AuthLoginWithPas from "./components/Author/AuthLoginWithPas";
+import Auth from "./pages/auth/auth";
 
 function App() {
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -14,7 +12,7 @@ function App() {
             <Router>
                 <Routes>
                     {/* Маршрут для страницы авторизации */}
-                    <Route path="/auth" element={isAuthenticated ? <Navigate to="/profile" /> : <AuthLoginWithPas />} />
+                    <Route path="/auth" element={isAuthenticated ? <Navigate to="/profile" /> : <Auth />} />
 
                     {/* Маршрут для страницы профиля */}
                     <Route path="/profile" element={isAuthenticated ? <MainLayout /> : <Navigate to="/auth" />} />
---
+++ src/pages/auth/auth.jsx
@@ -0,0 +1,97 @@
+import './auth.css';
+import { useDispatch } from "react-redux";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import { loginFailure, loginSuccess } from "../../redux/slice/auth";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+
+const Auth = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const schema = yup.object().shape({
+        email: yup.string().required().email(),
+        password: yup.string().required().min(6),
+    });
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const handleUserLogin = (uid) => {
+    };
+
+    const handleLoginError = (error) => {
+        console.error('Error:', error);
+    };
+
+    const signInWithEmail = async (data) => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, data.email, data.password);
+            const user = userCredential.user;
+            const userData = {
+                uid: user.uid,
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+                email: user.email
+            };
+            dispatch(loginSuccess(userData));
+            navigate('/profile');
+        } catch (error) {
+            handleLoginError(error);
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        try {
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+            const userData = {
+                uid: user.uid,
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+                email: user.email
+            };
+            dispatch(loginSuccess(userData));
+            navigate('/profile');
+        } catch (error) {
+            handleLoginError(error);
+        }
+    };
+
+    return (
+        <div className="main container-lg bg-white w-50 h-50">
+            <form onSubmit={handleSubmit(signInWithEmail)}>
+                <h5 className="d-flex align-self-start p-2">Авторизация</h5>
+                <div className="mb-3 row align-items-center">
+                    <label htmlFor="username" className="col-sm-4 col-form-label">Username:</label>
+                    <div className="col-sm-8">
+                        <input {...register("email")} className="form-control" type="text" id="username" />
+                    </div>
+                </div>
+                <div className="mb-3 row align-items-center">
+                    <label htmlFor="password" className="col-sm-4 col-form-label">Password:</label>
+                    <div className="col-sm-8">
+                        <input {...register("password")} className="form-control" type="password" id="password" />
+                    </div>
+                </div>
+                <div className="mb-3 d-flex align-items-left p-2">
+                    <input type="checkbox" className="form-check-input" id="remember" />
+                    <label className="form-check-label" htmlFor="remember">Remember me</label>
+                </div>
+                <div className='btn-sign'>
+                    <button type="submit" className="btn btn-primary">Sign in</button>
+                    <button onClick={signInWithGoogle} className="btn btn-danger">Sign with Google</button>
+                </div>
+
+            </form>
+
+        </div>
+    )
+}
+
+export default Auth;
